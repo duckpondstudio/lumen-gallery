1a3f54f7ac0c3001708266e19d863b9b84841e52
Nick Yonge <nick@curiobot.com>
2024-03-12 11:57:49 -0700

    GRB colour testing

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,9 @@ CRGB leds[40];
 
 void setup() {
   // put your setup code here, to run once:
-  FastLED.addLeds<WS2812B, 5>(leds, 40);
+  FastLED.addLeds<WS2812B, 5, GRB>(leds, 40);
   for (int i = 0; i < 40; i++) {
-      leds[i] = CRGB::Aquamarine;
+      leds[i] = CRGB::Red;
   }
   FastLED.show();
 }
