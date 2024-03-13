517ad053c5e49dda87f46f00af2f7655383ebffc
Nick Yonge <nick@curiobot.com>
2024-03-13 14:07:36 -0700

    a7 pindef

--- src/pindef.h
+++ src/pindef.h
@@ -8,6 +8,7 @@
 #define PIN_BTN_ENC 4
 
 #define PIN_ENC_A 2
-#define PIN_ENC_B 3
+// #define PIN_ENC_B 3
+#define PIN_ENC_B A7 // in theory 3/A7 are the same pin, using at84 ccw arduino pinmapping 
 
 #endif
