801d6e2dd521cdd9c970348ba7f6452c85bae987
Nick Yonge <nick@curiobot.com>
2024-03-12 20:43:33 -0700

    EncoderButton incompatible with board, gotta replace

--- src/input.cpp
+++ src/input.cpp
@@ -1,21 +1,25 @@
 #include "input.h"
 
 // EasyButton properties 
-EasyButton btnSat(PIN_BTN_SAT, BTNS_DEBOUNCE, BTNS_PULLUP, BTNS_INVERT);
-EasyButton btnVal(PIN_BTN_VAL, BTNS_DEBOUNCE, BTNS_PULLUP, BTNS_INVERT);
-EasyButton btnEnc(PIN_BTN_ENC, BTNS_DEBOUNCE, BTNS_PULLUP, BTNS_INVERT);
+// EasyButton btnSat(PIN_BTN_SAT, BTNS_DEBOUNCE, BTNS_PULLUP, BTNS_INVERT);
+// EasyButton btnVal(PIN_BTN_VAL, BTNS_DEBOUNCE, BTNS_PULLUP, BTNS_INVERT);
+// EasyButton btnEnc(PIN_BTN_ENC, BTNS_DEBOUNCE, BTNS_PULLUP, BTNS_INVERT);
+
+EncoderButton encoder = EncoderButton(PIN_ENC_A, PIN_ENC_B, PIN_BTN_ENC);
+EncoderButton btnSat = EncoderButton(PIN_BTN_SAT);
+EncoderButton btnVal = EncoderButton(PIN_BTN_VAL);
 
 void setupInput() {
-    btnSat.begin();
-    btnVal.begin();
-    btnEnc.begin();
+    // btnSat.begin();
+    // btnVal.begin();
+    // btnEnc.begin();
 }
 void loopInput() {
-    btnSat.read();
-    btnVal.read();
-    btnEnc.read();
+    encoder.update();
+    btnSat.update();
+    btnVal.update();
 }
 
-bool isPressedSat() { return btnSat.isPressed(); }
-bool isPressedVal() { return btnVal.isPressed(); }
-bool isPressedEnc() { return btnEnc.isPressed(); }
+bool isPressedSat() { return btnSat.buttonState(); }
+bool isPressedVal() { return btnVal.buttonState(); }
+bool isPressedEnc() { return encoder.buttonState(); }
--- src/input.h
+++ src/input.h
@@ -1,11 +1,14 @@
 #ifndef INPUT_H
 #define INPUT_H
 
-#include <EasyButton.h>
+#include <EncoderButton.h>
 
 #define PIN_BTN_SAT 7
 #define PIN_BTN_VAL 6
 #define PIN_BTN_ENC 4
+
+#define PIN_ENC_A 2
+#define PIN_ENC_B 3
 
 #define BTNS_DEBOUNCE 35
 #define BTNS_PULLUP false
