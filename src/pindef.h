957202f468a8363b404ff783d8e0e7bdcfc66b35
Nick Yonge <nick@curiobot.com>
2024-03-13 17:01:49 -0700

    pindef cleanup

--- src/pindef.h
+++ src/pindef.h
@@ -7,8 +7,7 @@
 #define PIN_BTN_VAL 6
 #define PIN_BTN_ENC 4
 
-#define PIN_ENC_A 2
-// #define PIN_ENC_B 3
-#define PIN_ENC_B A7 // in theory 3/A7 are the same pin, using at84 ccw arduino pinmapping 
+#define PIN_ENC_A 2 // INT0 / PCINT10
+#define PIN_ENC_B 3 // Non-int / PCIN7 / A7
 
 #endif
