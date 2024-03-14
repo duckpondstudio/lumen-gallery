87d5d33a4a1a1b8ac7e64d5ada7698fa6dc0e8ee
Nick Yonge <nick@curiobot.com>
2024-03-13 20:27:51 -0700

    wip buttons with denoise buffer

--- src/button.cpp
+++ src/button.cpp
@@ -1,9 +1,45 @@
 #include "button.h"
 
-Button::Button(int pin)
+Button::Button(int pin, bool pullup, int bufferSize = BUTTON_BUFFER_DEFAULT_SIZE)
+    : m_pin{pin},
+      m_bufferSize{bufferSize},
+      m_bufferLimit{bufferSize + BUTTON_BUFFER_LIMIT}
 {
+    pinMode(pin, pullup ? INPUT_PULLUP : INPUT);
 }
 
-void Button::loop() const
+void Button::loop()
 {
+    bool state = digitalRead(m_pin);
+
+    if (state)
+    {
+        if (m_buffer < m_bufferLimit)
+        {
+            bool latched = !pressed();
+            m_buffer += BUTTON_BUFFER_RISE;
+            if (!latched && pressed()) {
+                m_buffer += BUTTON_BUFFER_LATCH;
+            }
+            if (m_buffer > m_bufferLimit)
+            {
+                m_buffer = m_bufferLimit;
+            }
+        }
+    }
+    else
+    {
+        if (m_buffer > 0)
+        {
+            m_buffer -= BUTTON_BUFFER_FALL;
+            if (m_buffer < 0)
+            {
+                m_buffer = 0;
+            }
+        }
+    }
+}
+
+bool Button::pressed() {
+    return m_buffer >= m_bufferSize;
 }
--- src/button.h
+++ src/button.h
@@ -1,28 +1,41 @@
 #ifndef BUTTON_H
 #define BUTTON_H
 
+#include <Arduino.h>
+
 #ifndef BUTTON_BUFFER_LIMIT
-#define BUTTON_BUFFER_LIMIT 30
+#define BUTTON_BUFFER_LIMIT 25 // value above full buffer size to limit the buffer to
+#endif
+
+#ifndef BUTTON_BUFFER_DEFAULT_SIZE
+#define BUTTON_BUFFER_DEFAULT_SIZE 35 // default size used for a "full" buffer (eg, button is "on")
+#endif
+
+#ifndef BUTTON_BUFFER_LATCH
+#define BUTTON_BUFFER_LATCH 5 // extra value added to buffer upon reaching full buffer size, to avoid noise at the boundary between on/off
 #endif
 
 #ifndef BUTTON_BUFFER_RISE
-#define BUTTON_BUFFER_RISE 2
+#define BUTTON_BUFFER_RISE 2 // increment for buffer rising
 #endif
 
 #ifndef BUTTON_BUFFER_FALL
-#define BUTTON_BUFFER_FALL 3
+#define BUTTON_BUFFER_FALL 3 // decrement for buffer falling 
 #endif
 
 class Button
 {
     private:
-        int m_pin;
-        int m_bufferLimit;
+        const int m_pin;
+        const int m_bufferSize;
+        const int m_bufferLimit;
+
         int m_buffer;
 
     public:
-        Button(int pin);
-        void loop() const;
+        Button(int pin, bool pullup, int bufferSize);
+        void loop();
+        bool pressed();
 };
 
 #endif
