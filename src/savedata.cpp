f46d914bdb2f4aa0d461b61ceb2a5bfa0498b3b7
Nick Yonge <nick@curiobot.com>
2024-03-16 15:01:45 -0700

    getters in savedata.cpp

--- src/savedata.cpp
+++ src/savedata.cpp
@@ -4,18 +4,39 @@ saveData data;
 
 EEWL eewlData(data, BUFFER_LENGTH, BUFFER_START);
 
-void setupSaveData() {
-    // begin EEWL object 
+void setupSaveData()
+{
+    // begin EEWL object
     eewlData.begin();
 
-    // initial data get 
-    if (eewlData.get(data)) {
-        // data found 
-    } else {
-        // data not found, first run 
+    // initial data get
+    if (eewlData.get(data))
+    {
+        // data found
+    }
+    else
+    {
+        // data not found, first run
     }
 }
 
-bool hasSaved() {
+int getInt(int index)
+{
+    return data.intData[index];
+}
+void setInt(int index, int value, bool autoCommit = true)
+{
+}
+
+bool getBool(int index)
+{
+    return data.boolData[index];
+}
+void setBool(int index, bool value, bool autoCommit = true)
+{
+}
+
+bool hasSaved()
+{
     return data.saved;
 }
