36612ab7e32500f722e07d0fdce8f4e21a470783
Nick Yonge <nick@curiobot.com>
2024-03-16 15:00:25 -0700

    eewl script setup, first run

--- src/savedata.cpp
+++ src/savedata.cpp
@@ -5,6 +5,17 @@ saveData data;
 EEWL eewlData(data, BUFFER_LENGTH, BUFFER_START);
 
 void setupSaveData() {
-
+    // begin EEWL object 
     eewlData.begin();
+
+    // initial data get 
+    if (eewlData.get(data)) {
+        // data found 
+    } else {
+        // data not found, first run 
+    }
+}
+
+bool hasSaved() {
+    return data.saved;
 }
--- src/savedata.h
+++ src/savedata.h
@@ -3,22 +3,40 @@
 
 #include <eewl.h>
 
+#ifndef SAVE_LENGTH_BOOL
+#define SAVE_LENGTH_BOOL 2
+#endif
+
 #ifndef SAVE_LENGTH_INT
 #define SAVE_LENGTH_INT 10
 #endif
 
 #ifndef BUFFER_LENGTH
-#define BUFFER_LENGTH SAVE_LENGTH_INT + 1
+#define BUFFER_LENGTH SAVE_LENGTH_BOOL + (SAVE_LENGTH_INT * 2) + 1
 #endif
 
 #ifndef BUFFER_START
 #define BUFFER_START 0x10
 #endif
 
+// container for all local savedata
 struct saveData {
+    // have values previously been saved 
+    bool saved;
+    // array for all bools storage 
+    bool boolData[SAVE_LENGTH_BOOL];
+    // array for all ints storage 
     int intData[SAVE_LENGTH_INT];
 };
 
 void setupSaveData();
+
+int getInt(int index);
+void setInt(int index, int value, bool autoCommit = true);
+
+bool getBool(int index);
+void setBool(int index, bool value, bool autoCommit = true);
+
+bool hasSaved();
 
 #endif
