5d1d2259ce8a756ba81e29fc7cb02d268236a24f
Nick Yonge <nick@curiobot.com>
2024-03-16 14:47:01 -0700

    savedata definitions

--- src/savedata.cpp
+++ src/savedata.cpp
@@ -1,5 +1,10 @@
 #include "savedata.h"
 
+saveData data;
+
+EEWL eewlData(data, BUFFER_LENGTH, BUFFER_START);
+
 void setupSaveData() {
-    
+
+    eewlData.begin();
 }
--- src/savedata.h
+++ src/savedata.h
@@ -3,6 +3,22 @@
 
 #include <eewl.h>
 
+#ifndef SAVE_LENGTH_INT
+#define SAVE_LENGTH_INT 10
+#endif
+
+#ifndef BUFFER_LENGTH
+#define BUFFER_LENGTH SAVE_LENGTH_INT + 1
+#endif
+
+#ifndef BUFFER_START
+#define BUFFER_START 0x10
+#endif
+
+struct saveData {
+    int intData[SAVE_LENGTH_INT];
+};
+
 void setupSaveData();
 
 #endif
