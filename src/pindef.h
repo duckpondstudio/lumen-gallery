d4d9ee8c30cbbdb35b2336492662a2b2d0350d3c
Nick Yonge <nick@curiobot.com>
2024-09-20 13:06:54 -0700

    fixed pin definitions to follow v3 schematic

--- src/pindef.h
+++ src/pindef.h
@@ -1,13 +1,13 @@
 #ifndef PINDEF_H
 #define PINDEF_H
 
-#define PIN_LEDS 5
+#define PIN_LEDS 7
 
-#define PIN_BTN_SAT 7
+#define PIN_BTN_SAT 3
 #define PIN_BTN_VAL 6
-#define PIN_BTN_ENC 4
+#define PIN_BTN_ENC 5
 
-#define PIN_ENC_A 2 // INT0 / PCINT10
-#define PIN_ENC_B 3 // Non-int / PCIN7 / A7
+#define PIN_ENC_A 8 // INT0 / PCINT10
+#define PIN_ENC_B 4 // Non-int / PCINT4 / A4 / ADC4
 
 #endif
