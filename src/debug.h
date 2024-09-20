372a7ad3fee7e85f47c1971cf6ecdebf91f49e4f
Nick Yonge <nick@curiobot.com>
2024-09-19 18:48:15 -0700

    disable debug mode (i know this is a hacky way of handling debugging but idc it works)

--- src/debug.h
+++ src/debug.h
@@ -7,8 +7,8 @@
 #include "pindef.h"
 #include "leds.h"
 
-#define DEBUG_ENABLED true // use debug systems / tools
-#define DEBUG_ONLY true    // in main.h, if DEBUG_ENABLED, exclude setup and loop for non-debug functions
+#define DEBUG_ENABLED false // use debug systems / tools
+#define DEBUG_ONLY true     // in main.h, if DEBUG_ENABLED, exclude setup and loop for non-debug functions
 
 #define DEBUG_LED_CHIPSET WS2812B // chipset to use for debugging. Also see CHIPSET in leds.h
 #define DEBUG_LED_RGB_ORDER GRB   // RGB ordering to use for debugging. Also see RGB_ORDER in leds.h
@@ -23,10 +23,10 @@
 #define DEBUG_MULTI_BLINK_CYCLE false // cycle thru DEBUG_MULTI_PINS_SET with numbered blinks
 
 #define DEBUG_BASIC_LED true     // display a single LED
-#define DEBUG_BLINK_LED false     // blink a single LED
-#define DEBUG_LED_DATA_PIN 7      // data pin to use for a single LED
-#define DEBUG_LEDS_TEST_COUNT 38   // how many LEDs on the strip to test (default 1)
-#define DEBUG_LEDS_RAINBOW true   // if true, assigns all LEDs on the strip through HSV 0-255
+#define DEBUG_BLINK_LED false    // blink a single LED
+#define DEBUG_LED_DATA_PIN 7     // data pin to use for a single LED
+#define DEBUG_LEDS_TEST_COUNT 38 // how many LEDs on the strip to test (default 1)
+#define DEBUG_LEDS_RAINBOW true  // if true, assigns all LEDs on the strip through HSV 0-255
 #define DEBUG_RAINBOW_CYCLE true // if true, and if debugging rainbow, makes the rainbow animate
 
 #define DEBUG_HILOW_OUT true
